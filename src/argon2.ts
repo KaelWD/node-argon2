a3c03e2190593f00cf029caffdb09f54fe607dd5
Kael <kaelwd@gmail.com>
2025-10-31 21:03:34 +1100

    style: update formatter rules

--- src/argon2.ts
+++ src/argon2.ts
@@ -1,12 +1,12 @@
-import { randomBytes, timingSafeEqual, argon2 as _argon2 } from "node:crypto";
-import { promisify } from "node:util";
-import { deserialize, serialize } from "@phc/format";
-import type { Argon2Algorithm } from 'node:crypto';
+import type { Argon2Algorithm } from 'node:crypto'
+import { argon2 as _argon2, randomBytes, timingSafeEqual } from 'node:crypto'
+import { promisify } from 'node:util'
+import { deserialize, serialize } from '@phc/format'
 
-const generateSalt = promisify(randomBytes);
-const argon2 = promisify(_argon2);
+const generateSalt = promisify(randomBytes)
+const argon2 = promisify(_argon2)
 
-const types: readonly Argon2Algorithm[] = ['argon2i', 'argon2d', 'argon2id'];
+const types: readonly Argon2Algorithm[] = ['argon2i', 'argon2d', 'argon2id']
 
 const defaults = {
   hashLength: 32,
@@ -14,17 +14,17 @@ const defaults = {
   memoryCost: 1 << 16,
   parallelism: 4,
   type: 'argon2id',
-} satisfies Options;
+} satisfies Options
 
 interface Options {
-  hashLength?: number;
-  timeCost?: number;
-  memoryCost?: number;
-  parallelism?: number;
-  type?: Argon2Algorithm;
-  salt?: Buffer;
-  associatedData?: Buffer;
-  secret?: Buffer;
+  hashLength?: number
+  timeCost?: number
+  memoryCost?: number
+  parallelism?: number
+  type?: Argon2Algorithm
+  salt?: Buffer
+  associatedData?: Buffer
+  secret?: Buffer
 }
 
 interface PhcParams {
@@ -42,28 +42,37 @@ interface PhcOutParams extends PhcParams {
  * @param options The parameters for Argon2
  * @returns The hash generated from `password`
  */
-export async function hash(password: Buffer | string, options: Options & { raw: true }): Promise<Buffer>
-export async function hash(password: Buffer | string, options?: Options & { raw?: false }): Promise<string>
-export async function hash(password: Buffer | string, options?: Options & { raw?: boolean }): Promise<string | Buffer> {
-  let { raw, salt, ...rest } = { ...defaults, ...options };
+export async function hash(
+  password: Buffer | string,
+  options: Options & { raw: true },
+): Promise<Buffer>
+export async function hash(
+  password: Buffer | string,
+  options?: Options & { raw?: false },
+): Promise<string>
+export async function hash(
+  password: Buffer | string,
+  options?: Options & { raw?: boolean },
+): Promise<string | Buffer> {
+  let { raw, salt, ...rest } = { ...defaults, ...options }
 
   if (rest.hashLength > 2 ** 32 - 1) {
-    throw new RangeError("Hash length is too large");
+    throw new RangeError('Hash length is too large')
   }
 
   if (rest.memoryCost > 2 ** 32 - 1) {
-    throw new RangeError("Memory cost is too large");
+    throw new RangeError('Memory cost is too large')
   }
 
   if (rest.timeCost > 2 ** 32 - 1) {
-    throw new RangeError("Time cost is too large");
+    throw new RangeError('Time cost is too large')
   }
 
   if (rest.parallelism > 2 ** 24 - 1) {
-    throw new RangeError("Parallelism is too large");
+    throw new RangeError('Parallelism is too large')
   }
 
-  salt = salt ?? (await generateSalt(16));
+  salt = salt ?? (await generateSalt(16))
 
   const {
     hashLength,
@@ -73,7 +82,7 @@ export async function hash(password: Buffer | string, options?: Options & { raw?
     timeCost: t,
     parallelism: p,
     associatedData: data = Buffer.alloc(0),
-  } = rest;
+  } = rest
 
   const hash = await argon2(type, {
     message: password,
@@ -84,9 +93,9 @@ export async function hash(password: Buffer | string, options?: Options & { raw?
     passes: t,
     secret,
     associatedData: data,
-  });
+  })
   if (raw) {
-    return hash;
+    return hash
   }
 
   return serialize({
@@ -95,11 +104,11 @@ export async function hash(password: Buffer | string, options?: Options & { raw?
       m,
       t,
       p,
-      ...(data.byteLength > 0 ? { data } : {})
+      ...(data.byteLength > 0 ? { data } : {}),
     } satisfies PhcParams,
     salt,
     hash,
-  });
+  })
 }
 
 /**
@@ -107,29 +116,25 @@ export async function hash(password: Buffer | string, options?: Options & { raw?
  * @param options
  * @returns `true` if the digest parameters do not match the parameters in `options`, otherwise `false`
  */
-export function needsRehash(digest: string, options: {
-  timeCost?: number;
-  memoryCost?: number;
-  parallelism?: number;
-} = {}): boolean {
+export function needsRehash(
+  digest: string,
+  options: {
+    timeCost?: number
+    memoryCost?: number
+    parallelism?: number
+  } = {},
+): boolean {
   const { memoryCost, timeCost, parallelism } = {
     ...defaults,
     ...options,
-  };
+  }
 
-  const {
-    version: v,
-    params = {} as PhcParams,
-  } = deserialize(digest);
-  const { m, t, p } = params as PhcParams;
+  const { version: v, params = {} as PhcParams } = deserialize(digest)
+  const { m, t, p } = params as PhcParams
 
-  if (v) return true;
+  if (v) return true
 
-  return (
-    +m !== +memoryCost ||
-    +t !== +timeCost ||
-    +p !== +parallelism
-  );
+  return +m !== +memoryCost || +t !== +timeCost || +p !== +parallelism
 }
 
 /**
@@ -141,19 +146,14 @@ export function needsRehash(digest: string, options: {
 export async function verify(
   digest: string,
   password: Buffer | string,
-  options: { secret?: Buffer } = {}
+  options: { secret?: Buffer } = {},
 ): Promise<boolean> {
-  const {
-    id,
-    params = {} as PhcOutParams,
-    salt,
-    hash
-  } = deserialize(digest);
+  const { id, params = {} as PhcOutParams, salt, hash } = deserialize(digest)
 
-  const { m, t, p, data = '' } = params as PhcOutParams;
+  const { m, t, p, data = '' } = params as PhcOutParams
 
   if (!hash || !salt || !isArgon2Type(id)) {
-    return false;
+    return false
   }
 
   return timingSafeEqual(
@@ -165,14 +165,14 @@ export async function verify(
       memory: m,
       passes: t,
       secret: options.secret,
-      associatedData: Buffer.from(data, "base64"),
+      associatedData: Buffer.from(data, 'base64'),
     }),
     hash,
-  );
+  )
 }
 
-function isArgon2Type (str: string): str is Argon2Algorithm {
-  return types.includes(str as any);
+function isArgon2Type(str: string): str is Argon2Algorithm {
+  return types.includes(str as Argon2Algorithm)
 }
 
 export default {
